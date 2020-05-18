5da0158c3cee0ff5ed6ab667d6de20885161104e
deshnagandhi <deshnagandhi2002@gmail.com>
2020-05-18 14:16:11 +0530

    profile n registration updated

--- app.py
+++ app.py
@@ -1,28 +1,45 @@
 import hashlib
 from flask import Flask
-from flask import render_template,request, url_for, redirect, session
-from flask_pymongo import PyMongo
+from flask import render_template,request,url_for,redirect,session
 import hashlib
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
 
 mongo = PyMongo(app)
+from models import Users
+users_object =  Users()
+
+@app.route("/register_user")
+def register_user():
+    pass_object = {}
+    pass_object["full_name"] = "John Doe"
+    pass_object["email"] = "johndoe@gmail.com"
+    pass_object["password"] = "12345"
+    pass_object["password"] = hashlib.md5(pass_object["password"].encode()).hexdigest()
+    result = users_object.insert_user(data_object=pass_object)
+    if result:
+        return "inserted in databse"
+    else:
+        return "failed to insert"
+
 
 @app.route("/", methods = ["GET" , "POST"])
 def signin():
+    
     if request.method == "POST":
         if request.form["section_name"] == "login_form":
-            print("inside login form")
-            email=request.form["email"]
-            password=request.form["password"]
+            email = request.form["email"]
+            password = request.form["password"]
             password = hashlib.md5(password.encode()).hexdigest()
-            print(email,password)
+            result = users_object.signin_user(email,password)
+            if result:
+                return redirect("/blank")
+            else:
+                return render_template("signin.html",context="email id and password does not match")
         if request.form["section_name"] == "forgot_pass":
-            print("inside forgot password")
-            email=request.form["email"]
-            print(email)
-
+             email = request.form["email"] 
     return render_template('signin.html')
 
 @app.route("/blank",  methods = ["GET" , "POST"] )
@@ -31,7 +48,7 @@ def blank():
         email = request.form["email"]
         password = request.form["password"]
         password = hashlib.md5(password.encode()).hexdigest()
-        
+     
     return render_template('blank.html')
 
 @app.route("/teampage", methods = ["GET" , "POST"]) 
@@ -68,7 +85,9 @@ def profile():
         passed_object = {}
         for each in request.form:
             passed_object[each] = request.form[each]
-    return render_template('profile.html')
+        result=users_object.update_profile(email=session["email"] ,data_object=passed_object)
+    user = users_object.fetch_user(username=session["email"])
+    return render_template('profile.html',users_context=user)
 
 if __name__=='__main__':
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,28 @@
+from app import mongo
+from flask import session
+
+class Users:
+    def __init__(self):
+        self.mongo = mongo.db
+    def signin_user(self,email,password):
+        login_result = self.mongo.users.find_one({"$and":[{"email": email},{"password": password}]})
+        if login_result:
+            session["email"] = login_result["email"]
+            session["logged_in"] = True
+            session["id"] = str(login_result["_id"])
+            return True
+        else:
+            return False
+    
+    def insert_user(self,data_object):
+        return self.mongo.users.insert(data_object)
+
+    def fetch_user(self, username):
+        return self.mongo.users.find_one({"email":username})
+
+    def update_profile(self,data_object,email):
+        result = self.mongo.users.update_one({"email":email},{"$set":data_object})
+        if result:
+            return True
+        else:
+            return False
