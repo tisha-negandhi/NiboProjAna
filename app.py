f434ecd6a7e2842b54d0f8aa30bb4c6a6275ae48
Kunj Shah <kunjshah45>
2020-05-19 16:16:08 +0530

    . pulling code

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import hashlib
 from flask import Flask
-from flask import render_template,request
+from flask import render_template,request, url_for, redirect, session
 from flask_pymongo import PyMongo
 import hashlib
 
@@ -11,7 +11,21 @@ mongo = PyMongo(app)
 
 from models import Users
 
-user_object = Users()
+users_object = Users()
+
+@app.route("/register_user")
+def register_user():
+    pass_object = {}
+    pass_object["full_name"] = "John Doe"
+    pass_object["email"] = "johndoe@gmail.com"
+    pass_object["password"] = "12345"
+    pass_object["password"] = hashlib.md5(pass_object["password"].encode()).hexdigest()
+    result = users_object.insert_user(data_object=pass_object)
+    if result:
+        return "Inserted to database."
+    else:
+        return "Failed to insert to database"
+
 
 @app.route("/forgot_pass")
 def forgot_pass():
@@ -21,16 +35,16 @@ def forgot_pass():
 
 @app.route("/", methods = ["GET" , "POST"])
 def signin():
-    result = user_object.fetch_users()
-    for x in result:
-        print(x)
     if request.method == "POST":
         if request.form["section_name"] == "login_form":
-            print("inside login form ")
             email = request.form["email"]
             password = request.form["password"]
             password = hashlib.md5(password.encode()).hexdigest()
-            print(email, password)
+            result = users_object.signin_user(email, password)
+            if result:
+                return redirect("/blank")
+            else:
+                return render_template("signin.html", context="Email id and password doesn't match.")
         if request.form["section_name"] == "forgot_pass":
             print("inside forgot password")
             email = request.form["email"]
@@ -76,7 +90,9 @@ def profile():
         passed_object = {}
         for each in request.form:
             passed_object[each] = request.form[each]
-    return render_template('profile.html')
+        result = users_object.update_profile(email=session["email"], data_object=passed_object)
+    user = users_object.fetch_user(email=session["email"])
+    return render_template('profile.html', user_context=user)
 
 if __name__=='__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -5,5 +5,24 @@ class Users:
     def __init__(self):
         self.mongo = mongo.db
     
-    def fetch_users(self):
-        return self.mongo.users.find()
+    def signin_user(self, email, password):
+        login_result = self.mongo.users.find_one({"$and": [{"email": email},{"password": password}]})
+        if login_result:
+            session["email"] = login_result["email"]
+            session["logged_in"] = True
+            return True
+        else:
+            return False
+    
+    def insert_user(self, data_object):
+        return self.mongo.users.insert(data_object)
+    
+    def fetch_user(self, email):
+        return self.mongo.users.find_one({"email": email})
+    
+    def update_profile(self, email, data_object):
+        result = self.mongo.users.update_one({"email": email},{"$set":data_object})
+        if result:
+            return True
+        else:
+            return False
